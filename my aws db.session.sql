5f9ff10a90bbee842e4893ceccd5677459df1a15
irmaph <irmpal-1@student.ltu.se>
2025-02-19 21:01:28 +0100

    frontend registration logic

---
+++ my aws db.session.sql
@@ -0,0 +1,25 @@
+INSERT INTO login (
+    login_id,
+    login_role,
+    login_username,
+    login_password,
+    login_first_name,
+    login_last_name
+  )
+VALUES (
+    '1',
+    'admin',
+    'Irma1',
+    'admin',    
+    'Irma',
+    'PH'
+  ),
+
+  (
+    '2',
+    'admin',
+    'Vachana2',
+    'admin',
+    'Vachana',
+    'J'
+  );  
