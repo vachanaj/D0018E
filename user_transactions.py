cc8e3fb3ca87482fa781e03b966e06851cd82054
irmaph <irmpal-1@student.ltu.se>
2025-02-24 15:58:59 +0100

    stripe fixat

--- user_transactions.py
+++ user_transactions.py
@@ -4,29 +4,6 @@ import os
 import sqlite3
 from dotenv import load_dotenv
 
-
-#def update_tables(cust_cart):
-    # Update the tables
-    #cursor = db.cursor() 
-    #cursor.execute("DELETE FROM cart")
-   #cursor.execute("INSERT INTO transactions")
-
-    #db.commit()
-    #db.close()
-
-#   return True
-
-
-# Helper function for getting assets from asset table
-# def get_asset_details(asset_id):
-#     conn = sqlite3.connect('/home/irma/code/D0018E/database.db')
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT * FROM assets WHERE id=?", (asset_id,))
-#     asset_details = cursor.fetchone()
-#     conn.close()
-#     return asset_details 
-
-
 # Load environment variables
 load_dotenv()
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
