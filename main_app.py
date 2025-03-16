0b4f973c51be835c545ed8877bb6a57f8f4cf842
vachana <vach.java@gmail.com>
2025-03-16 11:54:42 +0100

    changes from Irma main branch

--- main_app.py
+++ main_app.py
@@ -270,12 +270,8 @@ def login_user():
         
         if user:
             # Store user information in session
-            session['userfirst'] = user['login_first_name']
-            session['userlast'] = user['login_last_name']
             session['username'] = user['login_username']
             session['role'] = user['login_role']
-            session['useremail'] = user['login_email']
-           
 
             # Check if the user is an admin or normal user
             if user['login_role'] == 'admin':
@@ -480,6 +476,31 @@ def get_cart_items():
 @app.route('/shoppingcart')
 def shoppingcart():
     return render_template('shoppingcart.html', public_key=PUBLIC_KEY)
+
+# @app.route('/create-checkout-session', methods=['POST'])
+# def create_checkout_session():
+#     try:
+#         session = stripe.checkout.Session.create(
+#             payment_method_types=['card'],
+#             line_items=[
+#                 {
+#                     'price_data': {
+#                         'currency': 'usd',
+#                         'product_data': {
+#                             'name': 'Sample Item'
+#                         },
+#                         'unit_amount': 2000,  # $20.00 in cents
+#                     },
+#                     'quantity': 1,
+#                 },
+#             ],
+#             mode='payment',
+#             success_url=request.host_url + 'checkout-success',
+#             cancel_url=request.host_url + 'shoppingcart',
+#         )
+#         return jsonify({'url': session.url})
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
 
 
 @app.route('/create-checkout-session', methods=['POST'])
